4b7cbda6b37b5931c18752daf4c585a79d6746f5
tsanak <gtsanak1@gmail.com>
2016-03-11 19:03:12 +0200

    Added command line arguments

--- generator.c
+++ generator.c
@@ -14,9 +14,16 @@
  */
 
 
+int checkArgs(char **argv);
 
 int main(int argc, char** argv) {
-    FILE *f = fopen("file.txt", "w");
+    char usage[] = "Usage: \n\t[File name to save the coordinates]\n\t[Number of coordinates that will be used]\n";
+
+    if(checkArgs(argv) == 1) {
+        printf("%s",usage);
+        return 1;
+    }
+    FILE *f = fopen(argv[1], "w");
     if (f == NULL) {
         printf("Error opening file!\n");
         exit(1);
@@ -24,11 +31,12 @@ int main(int argc, char** argv) {
     int utime;
     long int ltime;
     int i;
-    float cords[250][3];
+    int numberCoord = atol(argv[2]);
+    float cords[numberCoord][3];
     ltime = time(NULL);
     utime = (unsigned int) ltime / 2;
     srand(utime);
-    for (i = 0; i < 250; i++) {
+    for (i = 0; i < numberCoord; i++) {
         cords[i][0] = (float) 34 * rand() / (RAND_MAX - 1);
         cords[i][1] = (float) 34 * rand() / (RAND_MAX - 1);
         cords[i][2] = (float) 34 * rand() / (RAND_MAX - 1);
@@ -37,4 +45,16 @@ int main(int argc, char** argv) {
     fclose(f);
 
 }
+int checkArgs(char **argv) {
+    if(argv[1] == NULL) {
+        printf("File name to save the coordinates not specified.\n");
+        return 1;
+    }
 
+    if(argv[2] == NULL) {
+        printf("Number of coordinates that will be used not specified.\n");
+        return 1;
+    }
+
+    return 0;
+}
