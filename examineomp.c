a9fe061f295d080a797727984b3369a55ea958f6
adamantmc <adamantiosminas@gmail.com>
2016-03-22 14:48:15 +0200

    Read with fread.

--- examineomp.c
+++ examineomp.c
@@ -6,17 +6,21 @@
 #define lowLimit 12
 #define highLimit 30
 #define BUF_SIZE 5000000
+#define STR_LEN 31
+#define TOK_LEN 9
 
 long read(FILE *file, float **buffer, long limit);
+long read_new(FILE *file, char **buffer, long limit, long totalReadLines);
 int checkArgs(char **argv);
 void printTime(struct timespec start, struct timespec end);
+void parse(char *data, int floats, char returnBuffer[3][TOK_LEN]);
 
 int main(int argc, char** argv) {
-    float **buffer;
+    char **buffer = NULL;
     double start,t_total;
     FILE *data;
-    int i, openmp_threads, openmpi_processes;
-    long limit, readLines, runTime, total = 0;
+    int i, j, openmp_threads, openmpi_processes;
+    long limit, readLines, runTime, total = 0, sum = 0;
     char done = 0;
     char usage[] = "Usage: examine\n\t[number of collisions ( -1 as many as in file)]\n\t[maximum run time (-1 unlimited time)]\n\t[input file]\n\t[num of openmp threads (-1 all available threads)]\n\t[num of openmpi processes (-1 all available processes)]\n";
 
@@ -35,53 +39,64 @@ int main(int argc, char** argv) {
         printf("Could not open specified file.");
     }
 
-    buffer = malloc(sizeof(float *) * BUF_SIZE);
+    //Allocate memory for BUF_SIZE char pointers. Each char pointer will hold a line.
+    buffer = malloc(sizeof(char *) * BUF_SIZE);
     if(buffer == NULL) {
-        fprintf(stderr,"Could not allocate memory for buffer. Memory requested: %lu KBs.",BUF_SIZE * sizeof(float *));
+        fprintf(stderr,"Could not allocate memory for buffer. Memory requested: %lu KBs.",BUF_SIZE * sizeof(char *));
         return 1;
     }
+    //Allocate memory for each line
+    for(i=0; i<BUF_SIZE; i++) {
+        buffer[i] = malloc(sizeof(char) * STR_LEN);
+        if(buffer[i] == NULL) {
+            fprintf(stderr,"Could not allocate memory for string. Memory requested: %lu KBs.",STR_LEN*sizeof(char));
+        }
+    }
+
+    //char **cords = malloc(sizeof(char *)*3);
+    char cords[3][TOK_LEN];
+    float floats[3];
+    for(i=0; i<3; i++) buffer[i] = malloc(sizeof(char)*TOK_LEN);
 
     if(openmp_threads > 0)  omp_set_num_threads(openmp_threads);
     else if(openmp_threads == -1) omp_set_num_threads(omp_get_max_threads());
-    int sum=0;
+
     start = omp_get_wtime();
+
     while(!done) {
-        readLines = read(data, buffer, limit);
-        if(readLines == limit) {
-            printf("Limit reached.\n");
+        readLines = read_new(data, buffer, limit, total);
+
+        if(readLines != -1) total += readLines;
+        printf("Lines read so far: %ld | Lines read at latest read call: %ld\n",total, readLines);
+       
+        if(total == limit) {
+            printf("Limit reached. Read lines: %ld Total: %ld\n",readLines,total);
             done = 1;
         }
-       
+
         else if(readLines < BUF_SIZE) {
             printf("End of file reached\n");
             done = 1;
         }
-	#pragma omp parallel for shared(buffer),reduction(+:sum)
-	for(i=0;i<readLines;i++)
-	{
-	   	/*if(buffer[i][0]>=lowLimit && buffer[i][0]<=highLimit)
-		{
-			if(buffer[i][1]>=lowLimit && buffer[i][1]<=highLimit)
-			{
-				if(buffer[i][2]>=lowLimit && buffer[i][2]<=highLimit)
-				{
-					sum=sum+1;
-				}
-			}
-		}*/
-	
-	if(buffer[i][0]>=lowLimit && buffer[i][0]<=highLimit && buffer[i][1]>=lowLimit && buffer[i][1]<=highLimit && buffer[i][2]>=lowLimit && buffer[i][2]<=highLimit )
-	{
-		sum=sum+1;
-	}
-
-	}
-        printf("Debug: %ld %ld\n",total, readLines);
-        total += readLines;
+        #pragma omp parallel for shared(buffer) private(cords), reduction(+:sum)
+        for(i=0; i<readLines; i++)
+        {
+            parse(buffer[i],3,cords);
+            floats[0] = atof(cords[0]);
+            if(floats[0]>=lowLimit && floats[0]<=highLimit) {
+                floats[1] = atof(cords[1]);
+                if(floats[1] >= lowLimit && floats[1] <= highLimit) {
+                    floats[2] = atof(cords[2]);
+                    if(floats[2] >= lowLimit && floats[2] <= highLimit) {
+                        sum = sum + 1;
+                    }
+                }
+            }
+        }
     }
 
-t_total = omp_get_wtime() - start;
-printf("Total = %lf secs and %d pleiades in order \n" , t_total,sum);
+    t_total = omp_get_wtime() - start;
+    printf("Total = %lf secs \n" , t_total);
 
     fclose(data);
     return 0;
@@ -117,13 +132,38 @@ int checkArgs(char **argv) {
 long read(FILE *file, float **buffer, long limit) {
     int i, result;
     for(i=0; i<BUF_SIZE; i++) {
+        if(buffer[i] != NULL) free(buffer[i]);
         buffer[i] = (float *) malloc(sizeof(float) * 3);
         result = fscanf(file,"%f %f %f \n",&buffer[i][0],&buffer[i][1],&buffer[i][2]);
         if(result == EOF) return (i-1);
         if(i == limit) return limit;
-    
+
     }
     return BUF_SIZE;
+}
+
+long read_new(FILE *data, char **buffer, long limit, long totalReadLines) {
+    int result, i;
+    size_t length = STR_LEN;
+    for(i=0; i<BUF_SIZE; i++) {
+        //result =  getline(&buffer[i],&length,data);
+        result = fread(buffer[i], sizeof(char), length, data);
+        if(result < length) return (i-1);
+        if(i + totalReadLines == limit) return i;
+    }
+    return BUF_SIZE;
+}
+
+void parse(char *data, int floats, char returnBuffer[3][TOK_LEN]) {
+    int i,j;
+    //char buffer[floats][TOK_LEN];
+
+    for(i=0; i<TOK_LEN; i++) {
+        for(j=0; j<floats; j++) {
+            returnBuffer[j][i] = data[i + j*(TOK_LEN + 1)];
+        }
+    }
+    //for(j=0; j<floats; j++) returnBuffer[j] = buffer[j];
 }
 
 void printTime(struct timespec start, struct timespec end) {
@@ -136,5 +176,4 @@ void printTime(struct timespec start, struct timespec end) {
         timeElapsed_s--;
     }
     printf("Time: %ld.%09ld secs \n",timeElapsed_s,timeElapsed_n);
-
 }
